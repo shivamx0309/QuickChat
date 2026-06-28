d2a99913a4b7465c4352e63fa46b82b7915d7cd5
shivamx0903 <shivam99singhrock@gmail.com>
2026-06-28 14:52:45 +0530

    fix: Enable server port listening when deploying on Render

--- server/server.js
+++ server/server.js
@@ -79,7 +79,7 @@ app.use("/api/messages", messageRouter)
 // Connect to MongoDB
 await connectDB();
 
-if(process.env.NODE_ENV !== "production"){
+if(process.env.NODE_ENV !== "production" || process.env.RENDER){
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
 }
